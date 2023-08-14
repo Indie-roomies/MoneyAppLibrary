8bd2e82eca63bda477ad29e8ea09d453b29fa43d
d4niel-olaya <olayajuandaniel8@gmail.com>
2023-08-13 22:54:14 -0500

    parameter test

---
+++ Component1.razor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace MoneyAppLib
+{
+    public partial class Component1
+    {
+        [Parameter]
+        public string Name { get; set; }
+    }
+}
