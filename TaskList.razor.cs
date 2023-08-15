dc48da025b8ec98c9f0b3ec8acb4a8290cd8573e
d4niel-olaya <olayajuandaniel8@gmail.com>
2023-08-14 22:57:47 -0500

    cmp base

---
+++ TaskList.razor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace MoneyAppLib
+{
+    public partial class TaskList : ComponentBase
+    {
+
+        [Parameter]
+        public object? Model { get; set; }
+
+
+
+
+    }
+}
