68afd802ed8904ea63cf912916aa9cbbbe6dcdcc
d4niel-olaya <olayajuandaniel8@gmail.com>
2023-08-19 16:02:49 -0500

    modal component

---
+++ Components/Modal.razor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace MoneyAppLib.Components
+{
+    public partial class Modal
+    {
+
+        [Parameter]
+        public bool IsOpen { get;set; }
+
+        [Parameter]
+        public RenderFragment? DomContent { get; set; }
+    }
+}
